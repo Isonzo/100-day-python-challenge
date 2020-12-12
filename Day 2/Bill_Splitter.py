d795deea8e4f08097421abab25f3e02441f2b2a8
Isonzo <renato.soverina@gmail.com>
2020-12-12 13:04:10 -0500

    Final project for day 2, a bill splitter (tip included)

---
+++ Day 2/Bill_Splitter.py
@@ -0,0 +1,10 @@
+print("Welcome to the bill calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? "))
+people = int(input("How many people are splitting the bill? "))
+
+bill_tip = bill * (1 + tip/100)
+
+pay = round(bill_tip/people, 2)
+
+print(f"Everyone should pay ${pay}")
