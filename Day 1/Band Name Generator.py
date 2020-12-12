822d2635356c4b9b69b0894491f3dea4f6a98052
Isonzo <renato.soverina@gmail.com>
2020-12-11 20:12:42 -0500

    Final Project for Day 1

--- Day 1/Band Name Generator.py
+++ Day 1/Band Name Generator.py
@@ -1,2 +1,7 @@
 print("Welcome to the Band Name Generator")
-print("What's the name of the city you grew up in?")
+
+city = input("What's the name of the city you grew up in?\n")
+
+pet_name = input("What's your pet's name\n")
+
+print("How about " + city + " " + pet_name +"?")
