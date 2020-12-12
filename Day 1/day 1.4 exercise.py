7c60de86bc7dd9ce94323f130e44bc713f0e1339
Isonzo <renato.soverina@gmail.com>
2020-12-11 20:02:28 -0500

    Variable switcher

---
+++ Day 1/day 1.4 exercise.py
@@ -0,0 +1,24 @@
+#Don't change code below!
+
+a = input("a: ")
+
+b = input("b: ")
+
+#Don't change code above!
+
+#Switch a and b variables
+
+c = a
+d = b
+
+a = d
+b = c
+
+
+#Don't change code below!
+
+print("a = " + a)
+
+print("b = " + b)
+
+#Don't change code above!
