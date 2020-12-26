838e3bb00ccbb967c5512b91f2dd4c6bba71cafd
Isonzo <renato.soverina@gmail.com>
2020-12-26 15:05:57 -0500

    initial day 16 commit, have to get used to working with OOP

---
+++ Day 16/oop-coffee-machine-start/main.py
@@ -0,0 +1,34 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+def input_coins():
+    """Allows user to input coins and returns the total value"""
+    quarters = int(input("Input amount of quarters: "))*0.25
+    dimes = int(input("Input amount of dimes: "))*0.1
+    nickels = int(input("Input amount of nickels: "))*0.05
+    pennies = int(input("Input amount of pennies: "))*0.01
+
+    return quarters + dimes + nickels + pennies
+
+
+is_on = True
+
+menu = Menu()
+
+
+money_machine = MoneyMachine()
+
+coffee_machine = CoffeeMaker()
+
+while is_on:
+    prompt = input("What would you like? Espresso ($1.50), Latte ($2.50), Cappuccino ($3.00) : ").lower()
+
+    if prompt == "off":
+        quit()
+
+    elif menu.find_drink(prompt) != None:
+        pass
+        if menu.is_resource_sufficient(prompt):
+            order.name = prompt
+            payment = input_coins()
---
+++ Day 16/turtletest.py
@@ -0,0 +1,26 @@
+"""from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+timmy.shape("turtle")
+
+timmy.color("red")
+
+timmy.forward(100)
+
+
+my_screen = Screen()
+
+my_screen.exitonclick()"""
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon", ["Pikachu", "Squirtle", "Charmander"])
+
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+
+print(table)
