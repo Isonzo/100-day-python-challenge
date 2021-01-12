38de2e312da401702119691392744747faafecbe
Isonzo <renato.soverina@gmail.com>
2021-01-12 12:44:22 -0500

    Finally understand kind of

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -2,33 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-def input_coins():
-    """Allows user to input coins and returns the total value"""
-    quarters = int(input("Input amount of quarters: "))*0.25
-    dimes = int(input("Input amount of dimes: "))*0.1
-    nickels = int(input("Input amount of nickels: "))*0.05
-    pennies = int(input("Input amount of pennies: "))*0.01
-
-    return quarters + dimes + nickels + pennies
-
-
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 is_on = True
 
-menu = Menu()
-
-
-money_machine = MoneyMachine()
-
-coffee_machine = CoffeeMaker()
 
 while is_on:
-    prompt = input("What would you like? Espresso ($1.50), Latte ($2.50), Cappuccino ($3.00) : ").lower()
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
 
-    if prompt == "off":
-        quit()
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
 
-    elif menu.find_drink(prompt) != None:
-        pass    
-        if menu.is_resource_sufficient(prompt):
-            order.name = prompt
-            payment = input_coins()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
